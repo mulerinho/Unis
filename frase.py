bf65584802a5a5e6a69ca08087b5fe8780e7a5d6
mulerinho <alan_mulero@hotmail.com>
2023-10-28 10:16:24 -0300

    Adicionando modificação para misturar as letras em cada palavra.

--- frase.py
+++ frase.py
@@ -1,9 +1,11 @@
 # Retorne uma nova frase com cada palavra com as letras invertidas.
 from ntpath import split
-frase = input('Digite uma frase para ser invertida: ')
+frase = input('Digite uma frase para ter as palavras  invertidas: ')
 inverte = frase.split()
 inverte.reverse()
-print(inverte)
-
 nova = ' '.join(inverte)
 print(f'A frase invertida: ==>   {nova}')
+print('***********************************')
+print('Misturando as palavras e as letras nas palavras.')
+for i in inverte:
+    print(f'{i[::-1]}')
